a66ce5f8743a8054bacd54d8aaa7faf38f3afd9f
deppong <isaiahjdeppong@gmail.com>
2019-07-24 11:11:53 -0400

    added mkdir

--- imgpull.py
+++ imgpull.py
@@ -1,6 +1,10 @@
 import os
+import sys
 from urllib.request import urlopen, urlretrieve, Request
+from urllib.error import HTTPError
 from bs4 import BeautifulSoup
+
+sys.tracebacklimit = 0
 
 print("4chan.org Image scraper\n")
 
@@ -9,10 +13,14 @@ threadnum = input("What is the thread Number? ")
 
 url = "https://boards.4chan.org/%s/thread/%s" % (board, threadnum)
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-print(req)
 
 html = urlopen(req)
 soup = BeautifulSoup(html)
+
+try:
+    os.mkdir('images')
+except FileExistsError:
+    print("Directory 'images' already exists")
 
 imgs = soup.findAll("div", {"class":"fileText"})
 for img in imgs:
