1cbbb4fced84101e359b31b10ad4afc079e617c7
deppong <30512867+deppong@users.noreply.github.com>
2019-07-24 17:12:36 +0000

    added a folder sorting system

--- imgpull.py
+++ imgpull.py
@@ -6,10 +6,28 @@ from bs4 import BeautifulSoup
 
 sys.tracebacklimit = 0
 
+def folderPrompt():
+    check = str(input("Would you like to put the images in a seperate folder? (Y/N): ")).lower().strip()
+    try:
+        if check[0] == 'y':
+            return True
+        elif check[0] == 'n':
+            return False
+        else:
+            print('Invalid Input')
+            return ask_user()
+    except Exception as error:
+        print("Please enter valid inputs")
+        print(error)
+        return ask_user()
+
 print("4chan.org Image scraper\n")
 
 board = input("What board? ")
-threadnum = input("What is the thread Number? ")
+threadnum = input("What is the thread number? ")
+needFolder = folderPrompt()
+if needFolder:
+    folderName = input("What do you want to name the folder? ")
 
 url = "https://boards.4chan.org/%s/thread/%s" % (board, threadnum)
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
@@ -22,6 +40,13 @@ try:
 except FileExistsError:
     print("Directory 'images' already exists")
 
+if needFolder:
+    try:
+        os.mkdir('images/' + folderName)
+    except FileExistsError:
+        print("Directory '"  + folderName +  "' already exists")
+
+
 imgs = soup.findAll("div", {"class":"fileText"})
 for img in imgs:
     link = img.a['href'].split("imgurl=")[0]
@@ -29,5 +54,5 @@ for img in imgs:
     name = img.a.string
     
     print(name)
-    urlretrieve(link, 'images/' + name)
+    urlretrieve(link, 'images/' + folderName + '/' + name)
 
